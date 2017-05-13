ca50ddcd87bc87739d5982e6310017791da63388
Zhang <vincentzhang@ea.com>
2017-05-13 16:03:26 +0800

    Fixed performance issues in collision detection.

--- app/src/main/java/com/example/vincentzhang/Sprite/CollideDetector.java
+++ app/src/main/java/com/example/vincentzhang/Sprite/CollideDetector.java
@@ -1,13 +1,14 @@
 package com.example.vincentzhang.Sprite;
 
 import android.graphics.Rect;
+import android.util.Log;
 
 import com.example.vincentzhang.Sprite.imgemanagement.BitmapMask;
 import com.example.vincentzhang.Sprite.imgemanagement.Space4DTree;
 import com.example.vincentzhang.Sprite.imgemanagement.Space4DTreeNode;
 
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * Created by VincentZhang on 5/11/2017.
@@ -43,19 +44,28 @@ public class CollideDetector {
     }
 
     boolean detect(){
+        int detectCount = 0;
         BitmapMask targetBM = targetTree.getBmMask();
         Rect targetRect = targetTree.getRootRect(targetRowColumn);
 
         Rect colliderRect = colliderTree.getRootRect(colliderRowColumn);
 
-        Queue<Space4DTreeNode> nodeQueue = new LinkedBlockingQueue<>();
-        nodeQueue.offer(colliderTree.getRootNode(colliderRowColumn));
+        // Queue<Space4DTreeNode> nodeQueue = new LinkedBlockingQueue<>();
+        Stack<Space4DTreeNode> nodeStack = new Stack<>();
+        nodeStack.push(colliderTree.getRootNode(colliderRowColumn));
 
-        while(!nodeQueue.isEmpty()){
-            Space4DTreeNode curNode = nodeQueue.poll();
+        while(!nodeStack.isEmpty()){
+            detectCount++;
+            Space4DTreeNode curNode = nodeStack.pop();
             Rect colliderNodeSpaceRect = curNode.getRect();
             // Map to screen Rect
             Rect scrNodeRect = Utilities.mapRect(colliderRect, colliderNodeSpaceRect, colliderScrRect);
+
+            // Less than 4 pixels, regard as collided.
+            if(scrNodeRect.width() <= 2 && scrNodeRect.height() <= 2){
+                Log.i("Effort 1","Detected:" + detectCount + " times");
+                return true;
+            }
 
             // Consider mapped left coordinate.
             Rect targetImgColliderRect = Utilities.mapScrToImageRect(targetRect, targetScrRect, scrNodeRect);
@@ -68,23 +78,17 @@ public class CollideDetector {
 
             if(targetBM.getRangePixelCount(intersecRect) > 0){
                 if(curNode.isLeafNode()){
+                    Log.i("Effort 2","Detected:" + detectCount + " times");
                     return true;
                 }
 
-                Space4DTreeNode[] nodes = curNode.getChildren();
-                boolean hasChildNode = false;
+                ArrayList<Space4DTreeNode> nodes = curNode.getChildren();
                 for(Space4DTreeNode node : nodes){
-                    if(node != null){
-                        hasChildNode = true;
-                        nodeQueue.offer(node);
-                    }
-                }
-
-                if(!hasChildNode){
-                    return true;
+                    nodeStack.push(node);
                 }
             }
         }
+        Log.i("Effort 3","Not Detected collide! Effort:" + detectCount + " times");
         return false;
     }
 }
--- app/src/main/java/com/example/vincentzhang/Sprite/imgemanagement/Space4DTreeNode.java
+++ app/src/main/java/com/example/vincentzhang/Sprite/imgemanagement/Space4DTreeNode.java
@@ -7,6 +7,8 @@ import android.graphics.Rect;
 
 import com.example.vincentzhang.Sprite.Utilities;
 
+import java.util.ArrayList;
+
 /**
  * Created by VincentZhang on 5/11/2017.
  */
@@ -15,11 +17,16 @@ enum TreeNodeType {
     INNER, LEAF
 }
 
-public class Space4DTreeNode{
-    private Space4DTreeNode treeNodes[] = new Space4DTreeNode[4];
+public class Space4DTreeNode {
+    private ArrayList<Space4DTreeNode> treeNodes = new ArrayList<>();
     private Rect rect;
     private BitmapMask mask;
     private TreeNodeType type = TreeNodeType.INNER;
+    private int totalNodeCount = 1;
+
+    public int getTotalNodeCount() {
+        return totalNodeCount;
+    }
 
     public Space4DTreeNode(BitmapMask mask, Rect rect) {
         this.mask = mask;
@@ -34,24 +41,33 @@ public class Space4DTreeNode{
         // leftTop
         rects[0] = new Rect(rect.left, rect.top, rect.centerX(), rect.centerY());
         // rightTop
-        rects[1] = new Rect(rect.centerX(), rect.top, rect.right - 1, rect.centerY());
+        rects[1] = new Rect(rect.centerX(), rect.top, rect.right, rect.centerY());
         // leftDown
-        rects[2] = new Rect(rect.left, rect.centerY(), rect.centerX(), rect.bottom - 1);
+        rects[2] = new Rect(rect.left, rect.centerY(), rect.centerX(), rect.bottom);
         // rightDown
-        rects[3] = new Rect(rect.centerX(), rect.centerY(), rect.right - 1, rect.bottom - 1);
+        rects[3] = new Rect(rect.centerX(), rect.centerY(), rect.right, rect.bottom);
 
+        boolean hasChildNode = false;
         for (int i = 0; i < 4; i++) {
             if (mask.getRangePixelCount(rects[i]) > 0) {
-                treeNodes[i] = new Space4DTreeNode(mask, rects[i]);
+                Space4DTreeNode newNode = new Space4DTreeNode(mask, rects[i]);
+                treeNodes.add(newNode);
+                this.totalNodeCount += newNode.getTotalNodeCount();
+                hasChildNode = true;
             }
+        }
+
+        if (!hasChildNode) {
+            type = TreeNodeType.LEAF;
+            return;
         }
     }
 
-    public boolean isLeafNode(){
+    public boolean isLeafNode() {
         return type == TreeNodeType.LEAF;
     }
 
-    public Space4DTreeNode[] getChildren(){
+    public ArrayList<Space4DTreeNode> getChildren() {
         return this.treeNodes;
     }
 
@@ -64,16 +80,14 @@ public class Space4DTreeNode{
             p.setColor(Color.RED);
             canvas.drawRect(targetRect, p);
         } else {
-            for (int i = 0; i < 4; i++) {
-                if (treeNodes[i] != null) {
-                    Rect srcRect = Utilities.mapRect(rect, treeNodes[i].rect, targetRect);
-                    treeNodes[i].draw(canvas, level - 1, srcRect);
-                }
+            for (Space4DTreeNode node : treeNodes) {
+                Rect srcRect = Utilities.mapRect(rect, node.rect, targetRect);
+                node.draw(canvas, level - 1, srcRect);
             }
         }
     }
 
-    public Rect getRect(){
+    public Rect getRect() {
         return rect;
     }
 }
