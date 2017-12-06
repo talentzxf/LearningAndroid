5a61153f85c90cfc44b392061eebd89c06859c98
Zhang <vincentzhang@ea.com>
2017-12-06 09:51:16 +0800

    Progressive Texture, first try

--- app/src/main/java/com/example/vincentzhang/learnandroid/OpenGLRenderer.java
+++ app/src/main/java/com/example/vincentzhang/learnandroid/OpenGLRenderer.java
@@ -39,7 +39,7 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
 
         // Set the background frame color
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 
         mTriangle = new Triangle();
         mSquare = new Square();
--- app/src/main/java/com/example/vincentzhang/learnandroid/shapes/Cube.java
+++ app/src/main/java/com/example/vincentzhang/learnandroid/shapes/Cube.java
@@ -20,59 +20,80 @@ public class Cube {
     private FloatBuffer mVertexBuffer;
     private FloatBuffer mColorBuffer;
     private ByteBuffer mIndexBuffer;
+    private FloatBuffer mTextureBuffer;
 
     private float vertices[] = {
             -1.0f, -1.0f, -1.0f,
             1.0f, -1.0f, -1.0f,
             1.0f, 1.0f, -1.0f,
             -1.0f, 1.0f, -1.0f,
+
             -1.0f, -1.0f, 1.0f,
             1.0f, -1.0f, 1.0f,
             1.0f, 1.0f, 1.0f,
             -1.0f, 1.0f, 1.0f
     };
 
+    private float texCoords[] = {
+            0.0f, 0.0f,
+            1.0f, 0.0f,
+            1.0f, 1.0f,
+            0.0f, 1.0f,
+
+            0.0f, 1.0f,
+            1.0f, 1.0f,
+            1.0f, 0.0f,
+            0.0f, 0.0f,
+    };
+
     private float colors[] = {
-            1.0f, 0.0f, 0.0f, 1.0f,
-            1.0f, 0.0f, 0.0f, 1.0f,
-            1.0f, 0.0f, 0.0f, 1.0f,
-            1.0f, 0.0f, 0.0f, 1.0f,
-            1.0f, 0.0f, 0.0f, 1.0f,
-            1.0f, 0.0f, 0.0f, 1.0f,
-            1.0f, 0.0f, 0.0f, 1.0f,
-            1.0f, 0.0f, 0.0f, 1.0f
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f
     };
 
     private byte indices[] = {
-            0, 4, 5, 0, 5, 1,
-            1, 5, 6, 1, 6, 2,
-            2, 6, 7, 2, 7, 3,
-            3, 7, 4, 3, 4, 0,
-            4, 7, 6, 4, 6, 5,
-            3, 0, 1, 3, 1, 2
+            0, 4, 5, 0, 5, 1, // Bottom
+            1, 5, 6, 1, 6, 2, // Right
+            2, 6, 7, 2, 7, 3, // Top
+            3, 7, 4, 3, 4, 0, // LEFT
+            4, 7, 6, 4, 6, 5, // FRONT
+            3, 0, 1, 3, 1, 2  // BACK
     };
 
-    private float lightPos[] = {3.0f, 3.0f, -3.0f, 1.0f};
+
+    private float lightPos[] = {10.0f, 10.0f, 0.0f, 1.0f};
+    private float light2Pos[] = {-100.0f, -100.0f, 0.0f, 1.0f};
 
     private final String vertexShaderCode =
             // This matrix member variable provides a hook to manipulate
             // the coordinates of the objects that use this vertex shader
             "uniform mat4 projection;" +
+                    "const float poolHeight = 1.0;" +
                     "uniform mat4 model;" +
                     "uniform mat4 view;" +
                     "attribute vec4 vColor;" +
                     "attribute vec4 vPosition;" +
+                    "attribute vec2 vTexCoord;" +
                     "varying vec4 v_color;" +
                     "varying vec4 frag_pos;" +
                     "varying vec4 normal;" +
+                    "varying vec2 texCoord;" +
                     "void main() {" +
                     // The matrix must be included as a modifier of gl_Position.
                     // Note that the uMVPMatrix factor *must be first* in order
                     // for the matrix multiplication product to be correct.
-                    "  gl_Position = projection * view * model * vPosition;" +
+                    "gl_Position = projection * view * model * vPosition;" +
                     " frag_pos = model * vPosition;" +
                     " normal = vPosition;" +
                     "v_color = vColor;" +
+                    "texCoord = vTexCoord;" +
                     "}";
 
     private final String fragmentShaderCode =
@@ -80,22 +101,33 @@ public class Cube {
                     "varying vec4 frag_pos;" +
                     "varying vec4 normal;" +
                     "varying vec4 v_color;" +
+                    "varying vec2 texCoord;" +
                     "uniform vec4 lightPos;" +
+                    "uniform vec4 lightPos2;" +
                     "void main() {" +
                     " float ambientStrength = 0.2;" +
-                    " vec3 lightColor = vec3(0.5,0.5,0.5);" +
+                    " vec3 lightColor = vec3(1.0,1.0,1.0);" +
                     "vec3 ambient = ambientStrength * lightColor;" +
                     "vec3 norm = normalize(normal.xyz);" +
                     "vec3 lightDir = normalize(lightPos.xyz - frag_pos.xyz);" +
+                    "vec3 lightDir2 = normalize(lightPos2.xyz-frag_pos.xyz);" +
                     "float diff = max(dot(norm,lightDir),0.0);" +
+                    "float diff2 = max(dot(norm, lightDir2),0.0);" +
                     "vec3 diffuse = diff * lightColor * v_color.xyz;" +
                     "vec3 result = (ambient + diffuse).xyz; " +
+                    "if(texCoord[0] < 0.6 && texCoord[0] > 0.4 && texCoord[1]<0.6 && texCoord[1]>0.4){" +
+                    " result.r = 1.0;" +
+                    "}" +
                     "gl_FragColor = vec4(result,1.0);" +
                     "}";
+
+
+    int[] textureIDs = new int[1];   // Array for 1 texture-ID (NEW)
 
     private int program;
     private int mPositionHandle;
     private int mColorHandle;
+    private int mTexCoordHandle;
     private int mModelMatrix;
     private int mViewMatrix;
     private int mProjectionMatrix;
@@ -121,6 +153,12 @@ public class Cube {
         mIndexBuffer = ByteBuffer.allocateDirect(indices.length);
         mIndexBuffer.put(indices);
         mIndexBuffer.position(0);
+
+        byteBuf = ByteBuffer.allocateDirect(texCoords.length * 4);
+        byteBuf.order(ByteOrder.nativeOrder());
+        mTextureBuffer = byteBuf.asFloatBuffer();
+        mTextureBuffer.put(texCoords);
+        mTextureBuffer.position(0);
 
         // prepare shaders and OpenGL program
         int vertexShader = OpenGLRenderer.loadShader(
@@ -156,6 +194,9 @@ public class Cube {
         mColorHandle = GLES20.glGetAttribLocation(program, "vColor");
         OpenGLRenderer.checkGlError("glGetAttribLocation");
 
+        mTexCoordHandle = GLES20.glGetAttribLocation(program, "vTexCoord");
+        OpenGLRenderer.checkGlError("glGetAttribLocation");
+
         // Enable a handle to the triangle vertices
         GLES20.glEnableVertexAttribArray(mPositionHandle);
         // Prepare the triangle coordinate data
@@ -169,6 +210,11 @@ public class Cube {
         OpenGLRenderer.checkGlError("glGetUniformLocation");
         GLES20.glEnableVertexAttribArray(mColorHandle);
         OpenGLRenderer.checkGlError("glGetUniformLocation");
+
+        GLES20.glVertexAttribPointer(mTexCoordHandle, 2, GLES20.GL_FLOAT, false, 8, mTextureBuffer);
+        OpenGLRenderer.checkGlError("glGetUniformLocation");
+        GLES20.glEnableVertexAttribArray(mTexCoordHandle);
+
 
         // get handle to shape's transformation matrix
         mModelMatrix = GLES20.glGetUniformLocation(program, "model");
@@ -184,6 +230,10 @@ public class Cube {
         mLightPos = GLES20.glGetUniformLocation(program, "lightPos");
         OpenGLRenderer.checkGlError("glGetUniformLocation");
         GLES20.glUniform4fv(mLightPos, 1, this.lightPos, 0);
+
+        int mLightPos2 = GLES20.glGetUniformLocation(program, "lightPos2");
+        OpenGLRenderer.checkGlError("glGetUniformLocation");
+        GLES20.glUniform4fv(mLightPos2, 1, this.light2Pos, 0);
 
         // Apply the projection and view transformation
         GLES20.glUniformMatrix4fv(mModelMatrix, 1, false, model, 0);
@@ -202,5 +252,6 @@ public class Cube {
         // Disable vertex array
         GLES20.glDisableVertexAttribArray(mPositionHandle);
         GLES20.glDisableVertexAttribArray(mColorHandle);
+        GLES20.glDisableVertexAttribArray(mTexCoordHandle);
     }
 }
