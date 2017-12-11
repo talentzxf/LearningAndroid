fa5a3ddef154e3fe0cb12ba7b4a5c3280ab50032
vincentzhang <vincentzhang@ea.com>
2017-12-11 17:08:51 +0800

    Procedure marble texture

--- app/src/main/java/com/example/vincentzhang/learnandroid/shapes/Cube.java
+++ app/src/main/java/com/example/vincentzhang/learnandroid/shapes/Cube.java
@@ -96,8 +96,139 @@ public class Cube {
                     "texCoord = vTexCoord;" +
                     "}";
 
+//    private final String fragmentShaderCode =
+//            "precision mediump float;" +
+//                    "varying vec4 frag_pos;" +
+//                    "varying vec4 normal;" +
+//                    "varying vec4 v_color;" +
+//                    "varying vec2 texCoord;" +
+//                    "uniform vec4 lightPos;" +
+//                    "uniform vec4 lightPos2;" +
+//                    "uniform sampler2D u_Texture;" +    // The input texture.
+//                    "const float dimp_radius = 0.1;" +
+//                    "void main() {" +
+//                    " float ambientStrength = 0.3;" +
+//                    " vec3 lightColor = vec3(1.0,1.0,1.0);" +
+//                    "vec3 ambient = ambientStrength * lightColor;" +
+//                    "vec3 norm = normalize(normal.xyz);" +
+//                    // "norm = normalize(norm + 2.0*texture2D(u_Texture, texCoord).xyz -1.0);" +
+//                    "vec3 lightDir = normalize(lightPos.xyz - frag_pos.xyz);" +
+//                    "vec3 lightDir2 = normalize(lightPos2.xyz-frag_pos.xyz);" +
+//                    "float diff = max(dot(norm,lightDir),0.0);" +
+//                    "float diff2 = max(dot(norm, lightDir2),0.0);" +
+//                    "vec3 diffuse = diff * lightColor * v_color.xyz ;" +
+//                    "vec3 result = (ambient + diffuse).xyz * texture2D(u_Texture, texCoord).xyz; " +
+//
+//                    "gl_FragColor = vec4(result,1.0);" +
+//                    "}";
+
+    private final String noiseFuncions = "//\n" +
+            "// Description : Array and textureless GLSL 2D/3D/4D simplex \n" +
+            "//               noise functions.\n" +
+            "//      Author : Ian McEwan, Ashima Arts.\n" +
+            "//  Maintainer : stegu\n" +
+            "//     Lastmod : 20110822 (ijm)\n" +
+            "//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.\n" +
+            "//               Distributed under the MIT License. See LICENSE file.\n" +
+            "//               https://github.com/ashima/webgl-noise\n" +
+            "//               https://github.com/stegu/webgl-noise\n" +
+            "// \n" +
+            "\n" +
+            "vec3 mod289(vec3 x) {\n" +
+            "  return x - floor(x * (1.0 / 289.0)) * 289.0;\n" +
+            "}\n" +
+            "\n" +
+            "vec4 mod289(vec4 x) {\n" +
+            "  return x - floor(x * (1.0 / 289.0)) * 289.0;\n" +
+            "}\n" +
+            "\n" +
+            "vec4 permute(vec4 x) {\n" +
+            "     return mod289(((x*34.0)+1.0)*x);\n" +
+            "}\n" +
+            "\n" +
+            "vec4 taylorInvSqrt(vec4 r)\n" +
+            "{\n" +
+            "  return 1.79284291400159 - 0.85373472095314 * r;\n" +
+            "}\n" +
+            "\n" +
+            "float snoise(vec3 v)\n" +
+            "  { \n" +
+            "  const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;\n" +
+            "  const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);\n" +
+            "\n" +
+            "// First corner\n" +
+            "  vec3 i  = floor(v + dot(v, C.yyy) );\n" +
+            "  vec3 x0 =   v - i + dot(i, C.xxx) ;\n" +
+            "\n" +
+            "// Other corners\n" +
+            "  vec3 g = step(x0.yzx, x0.xyz);\n" +
+            "  vec3 l = 1.0 - g;\n" +
+            "  vec3 i1 = min( g.xyz, l.zxy );\n" +
+            "  vec3 i2 = max( g.xyz, l.zxy );\n" +
+            "\n" +
+            "  //   x0 = x0 - 0.0 + 0.0 * C.xxx;\n" +
+            "  //   x1 = x0 - i1  + 1.0 * C.xxx;\n" +
+            "  //   x2 = x0 - i2  + 2.0 * C.xxx;\n" +
+            "  //   x3 = x0 - 1.0 + 3.0 * C.xxx;\n" +
+            "  vec3 x1 = x0 - i1 + C.xxx;\n" +
+            "  vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y\n" +
+            "  vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y\n" +
+            "\n" +
+            "// Permutations\n" +
+            "  i = mod289(i); \n" +
+            "  vec4 p = permute( permute( permute( \n" +
+            "             i.z + vec4(0.0, i1.z, i2.z, 1.0 ))\n" +
+            "           + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) \n" +
+            "           + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));\n" +
+            "\n" +
+            "// Gradients: 7x7 points over a square, mapped onto an octahedron.\n" +
+            "// The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)\n" +
+            "  float n_ = 0.142857142857; // 1.0/7.0\n" +
+            "  vec3  ns = n_ * D.wyz - D.xzx;\n" +
+            "\n" +
+            "  vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)\n" +
+            "\n" +
+            "  vec4 x_ = floor(j * ns.z);\n" +
+            "  vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)\n" +
+            "\n" +
+            "  vec4 x = x_ *ns.x + ns.yyyy;\n" +
+            "  vec4 y = y_ *ns.x + ns.yyyy;\n" +
+            "  vec4 h = 1.0 - abs(x) - abs(y);\n" +
+            "\n" +
+            "  vec4 b0 = vec4( x.xy, y.xy );\n" +
+            "  vec4 b1 = vec4( x.zw, y.zw );\n" +
+            "\n" +
+            "  //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;\n" +
+            "  //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;\n" +
+            "  vec4 s0 = floor(b0)*2.0 + 1.0;\n" +
+            "  vec4 s1 = floor(b1)*2.0 + 1.0;\n" +
+            "  vec4 sh = -step(h, vec4(0.0));\n" +
+            "\n" +
+            "  vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;\n" +
+            "  vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;\n" +
+            "\n" +
+            "  vec3 p0 = vec3(a0.xy,h.x);\n" +
+            "  vec3 p1 = vec3(a0.zw,h.y);\n" +
+            "  vec3 p2 = vec3(a1.xy,h.z);\n" +
+            "  vec3 p3 = vec3(a1.zw,h.w);\n" +
+            "\n" +
+            "//Normalise gradients\n" +
+            "  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));\n" +
+            "  p0 *= norm.x;\n" +
+            "  p1 *= norm.y;\n" +
+            "  p2 *= norm.z;\n" +
+            "  p3 *= norm.w;\n" +
+            "\n" +
+            "// Mix final noise value\n" +
+            "  vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);\n" +
+            "  m = m * m;\n" +
+            "  return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), \n" +
+            "                                dot(p2,x2), dot(p3,x3) ) );\n" +
+            "}";
+
     private final String fragmentShaderCode =
             "precision mediump float;" +
+                    noiseFuncions +
                     "varying vec4 frag_pos;" +
                     "varying vec4 normal;" +
                     "varying vec4 v_color;" +
@@ -106,6 +237,30 @@ public class Cube {
                     "uniform vec4 lightPos2;" +
                     "uniform sampler2D u_Texture;" +    // The input texture.
                     "const float dimp_radius = 0.1;" +
+                    "const int roughness = 4;" +
+                    "float turbulence (vec3 P, int numFreq)" +
+                    "{" +
+                    "float val = 0.0;" +
+                    "float freq = 1.0;" +
+                    "for (int i=0; i<numFreq; i++) {" +
+                    "val += abs (snoise (P*freq) / freq);" +
+                    "freq *= 2.07;" +
+                    "}" +
+                    "return val;" +
+                    "}" +
+                    "  // Expects -1<x<1\n" +
+                    "  vec3 marble_color (float x)\n" +
+                    "  {\n" +
+                    "    vec3 col;\n" +
+                    "    x = sqrt(x);             // make x fall of rapidly...\n" +
+                    "    x = sqrt(x);\n" +
+                    "    x = sqrt(x);\n" +
+                    "    col = vec3(.2 + .75*x);  // scale x from 0<x<1 to 0.2<x<0.95\n" +
+                    "    col.b*=0.95;             // slightly reduce blue component (make color \"warmer\"):\n" +
+                    "    return col;\n" +
+                    "  }" +
+
+
                     "void main() {" +
                     " float ambientStrength = 0.3;" +
                     " vec3 lightColor = vec3(1.0,1.0,1.0);" +
@@ -117,11 +272,18 @@ public class Cube {
                     "float diff = max(dot(norm,lightDir),0.0);" +
                     "float diff2 = max(dot(norm, lightDir2),0.0);" +
                     "vec3 diffuse = diff * lightColor * v_color.xyz ;" +
-                    "vec3 result = (ambient + diffuse).xyz * texture2D(u_Texture, texCoord).xyz; " +
-
+                    // "vec3 result = (ambient + diffuse).xyz * texture2D(u_Texture, texCoord).xyz; " +
+                    "float freq = 10.0;" +
+                    "float t = 6.28 * texCoord.x;" +
+                    "t += turbulence(vec3(texCoord.xy,1.0), 4);" +
+                    "t=sin(t);" +
+                    "vec3 result = (ambient + diffuse).xyz * marble_color(sin(freq*t))*vec3(0.802, 0.794, 0.747);" +
+                    // "result *= vec3(sqrt(sin(freq * texCoord.x)),sqrt(sin(freq * texCoord.x)),sqrt(sin(freq * texCoord.x)));" +
+//                    " if(texCoord.y < texCoord.x*texCoord.x){" +
+//                    " result *= vec3(1.0,0.0,0.0); " +
+//                    "}"+
                     "gl_FragColor = vec4(result,1.0);" +
                     "}";
-
 
     int[] textureIDs = new int[1];   // Array for 1 texture-ID (NEW)
 
